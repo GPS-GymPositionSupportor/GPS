c32ac0e763af5b3a9f2f99471c25c369fbaf7025
문광희 <mgh7020@gmail.com>
2024-09-13 18:13:18 +0900

    feat : Base 코드 기반으로 Entity 클래스 작성

---
+++ src/main/java/gps/base/model/FavoriteId.java
@@ -0,0 +1,37 @@
+package gps.base.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+// 복합 Key를 위한 클래스
+public class FavoriteId implements Serializable {
+
+    private Long user;
+    private Long gym;
+
+    // Constructor
+    public FavoriteId() {
+    }
+
+    public FavoriteId(Long user, Long gym) {
+        this.user = user;
+        this.gym = gym;
+    }
+
+
+    // method
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FavoriteId that = (FavoriteId) o;
+        return Objects.equals(user, that.user) && Objects.equals(gym, that.gym);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(user, gym);
+    }
+}
