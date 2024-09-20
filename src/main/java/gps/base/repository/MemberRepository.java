30d7b410e394d19acf40e8e015135d1d73419219
adward27 <whalsrnr2741@gmail.com>
2024-09-20 23:21:27 +0900

    test : 지도 API 테스트

--- src/main/java/gps/base/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package gps.base.repository;
-
-import gps.base.model.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
