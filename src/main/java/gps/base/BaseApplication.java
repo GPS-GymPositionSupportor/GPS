12073d8393a0829c89b8d1efd3424a1810bc7d08
Tory99 <hjw990826@gmail.com>
2024-09-22 10:31:30 +0900

    Update BaseApplication.java

--- src/main/java/gps/base/BaseApplication.java
+++ src/main/java/gps/base/BaseApplication.java
@@ -2,8 +2,9 @@ package gps.base;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class BaseApplication {
 
     public static void main(String[] args) {
