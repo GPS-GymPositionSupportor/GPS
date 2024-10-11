38ebc6b7079c0e306f48e3ca71f0bacd94e8cb2f
adward27 <whalsrnr2741@gmail.com>
2024-10-11 21:58:09 +0900

    test : DB 기능

---
+++ src/main/java/gps/base/gymdata/DTO/GymDTO.java
@@ -0,0 +1,68 @@
+package gps.base.gymdata.DTO;
+
+public class GymDTO {
+    private String gName;
+    private String address;
+    private double gLongitude;
+    private double gLatitude;
+    private String information;
+    private String gymImage;
+    private Byte rating;
+
+    // Getters and setters
+    public String getGName() {
+        return gName;
+    }
+
+    public void setGName(String gName) {
+        this.gName = gName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getGLongitude() {
+        return gLongitude;
+    }
+
+    public void setGLongitude(double gLongitude) {
+        this.gLongitude = gLongitude;
+    }
+
+    public double getGLatitude() {
+        return gLatitude;
+    }
+
+    public void setGLatitude(double gLatitude) {
+        this.gLatitude = gLatitude;
+    }
+
+    public String getInformation() {
+        return information;
+    }
+
+    public void setInformation(String information) {
+        this.information = information;
+    }
+
+    public String getGymImage() {
+        return gymImage;
+    }
+
+    public void setGymImage(String gymImage) {
+        this.gymImage = gymImage;
+    }
+
+    public Byte getRating() {
+        return rating;
+    }
+
+    public void setRating(Byte rating) {
+        this.rating = rating;
+    }
+}
--- src/main/java/gps/base/gymdata/controller/MainController.java
+++ src/main/java/gps/base/gymdata/controller/MainController.java
@@ -1,7 +1,7 @@
 package gps.base.gymdata.controller;
 
-import gps.base.GymService;
-import gps.base.gymdata.model.Gym;
+import gps.base.gymdata.DTO.GymDTO;
+import gps.base.gymdata.KakaoGymCrawler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,24 +12,12 @@ import java.util.List;
 @RequestMapping("/api")
 public class MainController {
 
-    private final GymService gymService;
-
     @Autowired
-    public MainController(GymService gymService) {
-        this.gymService = gymService;
-    }
+    private KakaoGymCrawler kakaoGymCrawler;
 
-    // 헬스장 데이터를 가져오는 GET 요청
-    @GetMapping("/gyms")
-    public ResponseEntity<List<Gym>> getAllGyms() {
-        List<Gym> gyms = gymService.getAllGyms();
+    @GetMapping("/gyms/crawl")
+    public ResponseEntity<List<GymDTO>> crawlGyms() {
+        List<GymDTO> gyms = kakaoGymCrawler.crawlAllGyms();
         return ResponseEntity.ok(gyms);
-    }
-
-    // 헬스장 데이터를 저장하는 POST 요청
-    @PostMapping("/gyms")
-    public ResponseEntity<String> saveGyms(@RequestBody List<Gym> gyms) {
-        gymService.saveGyms(gyms);
-        return ResponseEntity.ok("헬스장 데이터가 성공적으로 저장되었습니다.");
     }
 }
