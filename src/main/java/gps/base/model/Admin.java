67d82b8068dd1f62f6ef7cf3166aaff0ebc13388
adward27 <whalsrnr2741@gmail.com>
2024-10-16 13:03:07 +0900

    test : DB 기능

---
+++ src/main/java/gps/base/model/Admin.java
@@ -0,0 +1,21 @@
+package gps.base.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "admin")
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "admin_id")
+    private Long adminId;
+
+    @JoinColumn(name = "user_id")
+    private Long userId;
+}
