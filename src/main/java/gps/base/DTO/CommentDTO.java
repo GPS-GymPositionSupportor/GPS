9eeb07273200aaafc36e1fb9f34a3c7f1001a75e
문광희 <mgh7020@gmail.com>
2024-10-23 00:47:46 +0900

    refactor : 어노테이션 변경

--- src/main/java/gps/base/DTO/CommentDTO.java
+++ src/main/java/gps/base/DTO/CommentDTO.java
@@ -1,11 +1,11 @@
 package gps.base.DTO;
 
 import gps.base.model.Member;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+@Data
 public class CommentDTO {
     private Long userId;
     private Long gymId;
