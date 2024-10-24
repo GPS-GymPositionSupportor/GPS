1b74b8b19b1e49b2c471335ef6ebb7e5ffcbae2c
문광희 <mgh7020@gmail.com>
2024-10-24 21:52:59 +0900

    rm : 코드 merge 로 인한 불필요

--- src/main/java/gps/base/exception/GymNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package gps.base.exception;
-
-public class GymNotFoundException extends RuntimeException {
-    public GymNotFoundException(Long gymId) {
-        super(gymId + " 에 해당하는 체육관을 찾을 수 없습니다.");
-    }
-}
