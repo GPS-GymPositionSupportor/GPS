f5e87b78634b89f33ee7bf1f19848f4a02ab40f8
문광희 <mgh7020@gmail.com>
2024-10-25 00:50:32 +0900

    feat : Controller 코드 수정

--- src/main/java/gps/base/controller/MainController.java
+++ src/main/java/gps/base/controller/MainController.java
@@ -11,7 +11,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
