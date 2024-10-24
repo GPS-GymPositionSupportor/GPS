42e5d2eeb28a16887248b2d31c877484016abe52
문광희 <mgh7020@gmail.com>
2024-10-24 21:53:49 +0900

    feat : Image Entity 추가

--- src/main/java/gps/base/model/Image.java
+++ src/main/java/gps/base/model/Image.java
@@ -3,6 +3,7 @@ package gps.base.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Image {
 
     @Id
