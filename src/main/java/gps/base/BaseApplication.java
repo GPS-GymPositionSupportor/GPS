aa288fd1543bd7284d39ec64cf3f54115158e43e
문광희 <mgh7020@gmail.com>
2024-12-07 15:51:01 +0900

    rm : test

--- src/main/java/gps/base/BaseApplication.java
+++ src/main/java/gps/base/BaseApplication.java
@@ -3,6 +3,8 @@ package gps.base;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class BaseApplication {
 
